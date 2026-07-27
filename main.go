31416f27e3547249819ffc38df1df53b8066524c
babadyk412 <306774848+babadyk412@users.noreply.github.com>
2026-07-27 14:35:12 +0300

    2 структура ввода пароля. списко паролей. меню

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type PasswordEntry struct{
+	Site string
+	Login string
+	Password string
+}
+
+type PasswordManager struct{
+	Passwords map[string]PasswordEntry
+}
+
+func main() {
+
+fmt.Println("======================")
+fmt.Println("Password Manager")
+fmt.Println("======================")
+
+fmt.Println("1 добавить пароль")
+fmt.Println("2 найти пароль")
+fmt.Println("3 показать все сайты")
+fmt.Println("4 удалить пароль")
+fmt.Println("5 сгенерировать пароль")
+fmt.Println("6 выход")
+
+}
+
+
