fd833e102f7aeba2d338f5d535099a0581042c58
babadyk412 <306774848+babadyk412@users.noreply.github.com>
2026-07-27 16:39:18 +0300

    gt

--- main.go
+++ main.go
@@ -2,61 +2,62 @@ package main
 
 import "fmt"
 
-type PasswordEntry struct{
-	Site string
-	Login string
+type PasswordEntry struct {
+	Site     string
+	Login    string
 	Password string
 }
 
-type PasswordManager struct{
+type PasswordManager struct {
 	Passwords map[string]PasswordEntry
 }
 
-
 func main() {
 
+	pas := PasswordManager{
+		Passwords: make(map[string]PasswordEntry),
+	}
 
+	fmt.Println("======================")
+	fmt.Println("Password Manager")
+	fmt.Println("======================")
 
-fmt.Println("======================")
-fmt.Println("Password Manager")
-fmt.Println("======================")
+	fmt.Println("1 добавить пароль")
+	fmt.Println("2 найти пароль")
+	fmt.Println("3 показать все сайты")
+	fmt.Println("4 удалить пароль")
+	fmt.Println("5 сгенерировать пароль")
+	fmt.Println("6 выход")
 
-fmt.Println("1 добавить пароль")
-fmt.Println("2 найти пароль")
-fmt.Println("3 показать все сайты")
-fmt.Println("4 удалить пароль")
-fmt.Println("5 сгенерировать пароль")
-fmt.Println("6 выход")
+	fmt.Println("======================")
+	fmt.Println("что делать")
+	var uu int
+	fmt.Scan(&uu)
 
-fmt.Println("======================")
-fmt.Println("что делать")
-var uu int
-fmt.Scan(&uu)
+	if uu == 1 {
+		pas.addPasword()
+	}
 
-if uu==1{
-	addPasword()
 }
 
-}
-
-func(p *PasswordManager) addPasword(ff PasswordEntry){
+func (p *PasswordManager) addPasword() {
 	var entry PasswordEntry
 
 	fmt.Println("добавить сайт")
-	var tt string
-	fmt.Scan(tt)
-	entry.Site=tt
-	
+	var site string
+	fmt.Scan(&site)
+	entry.Site = site
+
 	fmt.Println("добавить логин")
-	var ttt string
-	fmt.Scan(ttt)
-	entry.Login=ttt
-	
+	var login string
+	fmt.Scan(&login)
+	entry.Login = login
+
 	fmt.Println("добавить пароль")
-	var t string
-	fmt.Scan(t)
-	entry.Password=t
+	var pasword string
+	fmt.Scan(&pasword)
+	entry.Password = pasword
+	p.Passwords[site] = entry
+
 }
-
-
 
